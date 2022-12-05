5bedfab44127b3f39861f416eca59d1b575ed9ad
Felix Bickelmann <felix.bickelmann@n4.de>
2022-12-05 14:45:36 +0100

    day5 c2

--- src/challenges/aoc2022/day5/Day5.java
+++ src/challenges/aoc2022/day5/Day5.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -19,23 +20,28 @@ public class Day5 {
         BufferedReader br = new BufferedReader(new FileReader("src/challenges/aoc2022/day5/day5-data.txt"));
 
         int lineCount = 0;
+        boolean challenge2 = false;
 
         String st;
         while ((st = br.readLine()) != null) {
             lineCount++;
             if (lineCount <= length) {
-//                System.out.println(st);
                 getContainer(st);
             }
             if (lineCount > length + 2) {
-                moveContainer(st);
+                moveContainer(st, challenge2);
             }
         }
 
-        System.out.println(ship);
+        StringBuilder res = new StringBuilder();
+
+        for (int i = 0; i < ship.size(); i++) {
+            res.append(ship.get(i + 1).get(0).replace("[", "").replace("]", ""));
+        }
+        System.out.println(res);
     }
 
-    private static void moveContainer(String st) {
+    private static void moveContainer(String st, boolean c2) {
         List<Integer> movement = getMovement(st);
         int move = movement.get(0);
         int from = movement.get(1);
@@ -48,6 +54,10 @@ public class Day5 {
             fromContainer.remove(0);
         }
         ship.put(from, fromContainer);
+
+        if (c2) {
+            Collections.reverse(containerToMovie);
+        }
 
         List<String> toContainer = ship.get(to);
         for (String s : containerToMovie) {
